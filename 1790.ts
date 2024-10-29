0f91b22dedf56b255d2257758187f77acc0c4d50
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2024-10-29 19:47:05 +0900

    Update 1790.ts

--- 1790.ts
+++ 1790.ts
@@ -11,8 +11,39 @@ const INF = Number.MAX_SAFE_INTEGER
 rl.on('line', (line: string) => {
     inputLines.push(line);
 }).on('close', () => {
-    const [ N ] = inputLines[idx++].split(' ').map(Number)
-    for (let i = N; i >= 1; i--) {
-        console.log(i)
+    let [ N , K ] = inputLines[idx++].split(' ').map(Number)
+    const total = []
+    for (let i = 1; 9*(10**(i-1)) <= 1000000000; i++) {
+        total.push((i)*9*(10**(i-1)))
+    }
+    for (let i = 1 ; i < total.length; i++) {
+        total[i] = total[i-1] + total[i]
+    }
+    let num
+    for (let i = 0; i < total.length; i ++) {
+        if (total[i] >= K) {
+            num = i
+            break
+        }
+    }
+    const minus = num > 0 ? total[num-1] : 0
+    K-=minus
+    let numbers = 0
+    let start = 10**num
+    let cnt = 0
+    for (let i = 10**(num); i < 10**(num+1); i++) {
+        cnt += num+1
+        start += 1
+        if (cnt >= K) {
+            numbers = i
+            break
+        }
+
+    }
+    if (start-1 <= N)  {
+        console.log(String(numbers)[K+num-cnt])
+    }
+    else {
+        console.log(-1)
     }
 });
